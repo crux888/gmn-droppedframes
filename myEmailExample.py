53e4c2af6240b2ace97b84fdd2869783e8807c3c
David B <88885429+crux888@users.noreply.github.com>
2023-06-19 08:06:35 +0100

    Gmail app passwords

--- myEmailExample.py
+++ myEmailExample.py
@@ -2,6 +2,14 @@ import os
 
 # Set environment variables
 
+# You'll need a Gmail address and an 'app password' to send emails.
+#
+# See this article from Google for more information:
+# https://support.google.com/accounts/answer/185833?hl=en
+#
+# Note that Google does not recommend the use of 'app passwords' 
+# and can sometimes change how they work.
+
 def setVar():
     os.environ['EMAIL_TO'] = 'yourname@domain.com'
     os.environ['EMAIL_FROM'] = 'yourname@domain.com'
